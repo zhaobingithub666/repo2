4ae80537c0c0afcf74f65f9e479e353b7111f45e
zhangsan <zhangsan@itcast.com>
2019-06-29 23:13:44 +0800

    commit

--- git1/src/com/itheima/git/GitTest.java
+++ git1/src/com/itheima/git/GitTest.java
@@ -10,5 +10,10 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("sss");
+        System.out.println("sss");
+        System.out.println("sss");
+        System.out.println("sss");
+        System.out.println("sss");
     }
+
 }
