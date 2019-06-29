2863e6eac81a15230858725be3962f10926269d3
zhangsan <zhangsan@itcast.com>
2019-06-29 20:33:47 +0800

    创建一个git

---
+++ git1/src/com/itheima/git/GitTest.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+public class GitTest {
+    public void test(){
+
+        System.out.println("22");
+        System.out.println("22");
+        System.out.println("22");
+    }
+}
